1325ce7228183084451853930c7c7e9b8a4ed692
HoangTuan <tuanhoang2801@gmail.com>
2024-09-25 15:12:31 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <button className="btn btn-primary">test</button>
+        </div>
+    );
+};
+
+export default App;
